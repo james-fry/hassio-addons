378ecd9cccf1207078f5606368721448d6578ea6
Bouwe Westerdijk <bouwe@westerdijk.info>
2019-07-14 10:33:29 +0200

    Update ink2mqtt.sh

--- ink2mqtt/ink2mqtt.sh
+++ ink2mqtt/ink2mqtt.sh
@@ -58,13 +58,13 @@ echo
 while true; do
   mapfile -t lines < <(ink -b bjnp://$PRINTERIP)
   if [ "${lines[2]}" != "" ]; then
-     payload="{ \"Device: \"${lines[2]}\""
+     payload='{ \"Device: \"${lines[2]}\"'
      numlines=${#lines[@]}
      for (( i=4; i<=$numlines-1; i++ ))
      do
-       payload=$payload", \"${lines[i]%\%}\""
+       payload=$payload', \"${lines[i]%\%}\"'
      done
-     payload=$payload" }"
+     payload=$payload' }'
      datetime=`date`
      echo $datetime " -- " $payload | sed -e 's/: /": /g'
      #echo $payload | sed -e 's/: /": /g'  | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i ink2mqtt -r -l -t ink2mqtt/"$BRAND""$TYPE"
