7aad7fd73ac01f5dbf884732bacb610fc96dc8de
Bouwe Westerdijk <bouwe@westerdijk.info>
2019-07-14 09:17:50 +0200

    Pick up the "series" designation, when present, from the ink-command

--- ink2mqtt/ink2mqtt.sh
+++ ink2mqtt/ink2mqtt.sh
@@ -18,6 +18,7 @@ if [ "${lines[2]}" != "" ]; then
    PRINTER=${lines[2]}
    BRAND=$(echo $PRINTER | awk -F' ' '{ print $1 }')
    TYPE=$(echo $PRINTER | awk -F' ' '{ print $2 }')
+   EXTRA=$(echo $PRINTER | awk -F' ' '{ print $3 }')
    json_attributes=()
    numlines=${#lines[@]}
     for (( i=4; i<=$numlines-1; i++ ))
@@ -33,7 +34,8 @@ echo "Printer IP address =" $PRINTERIP
 echo "Sleep interval =" $INTERVAL
 echo "Brand = " $BRAND
 echo "Type = " $TYPE
-echo
+echo "Extra = " $EXTRA
+echo ""
 #set -x  ## uncomment for MQTT logging...
 
 echo "MQTT autodiscovery for all ink colours:"
@@ -41,8 +43,7 @@ echo "MQTT autodiscovery for all ink colours:"
 for i in "${json_attributes[@]}"
 do
   echo "$i"
-  AUTO_D="{\"unit_of_measurement\":\"%\",\"icon\":\"mdi:water\",\"value_template\":\"{{ value_json.$i }}\",\"state_topic\":\"ink2mqtt/"$BRAND""$TYPE"\",\"name\":\"$BRAND $TYPE $i Ink Level\",\"unique_id\":\"$BRAND $TYPE series_"$i"_ink2mqtt\",\"device\":{\"identifiers\":\"$BRAND $TYPE series\",\"name\":\"$BRAND $TYPE series\",\"sw_version\":\"2.020\",\"model\":\"$TYPE 
-#series\",\"manufacturer\":\"$BRAND\"}}"
+  AUTO_D="{\"unit_of_measurement\":\"%\",\"icon\":\"mdi:water\",\"value_template\":\"{{ value_json.$i }}\",\"state_topic\":\"ink2mqtt/"$BRAND""$TYPE"\",\"name\":\"$BRAND $TYPE $i Ink Level\",\"unique_id\":\"$BRAND $TYPE $EXTRA_"$i"_ink2mqtt\",\"device\":{\"identifiers\":\"$BRAND $TYPE $EXTRA\",\"manufacturer\":\"$BRAND",\"model\":\"$TYPE $EXTRA"}}"
   echo $AUTO_D
   echo $AUTO_D | mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i ink2mqtt -r -l -t homeassistant/sensor/"$BRAND"_"$TYPE"/$i/config
   echo
